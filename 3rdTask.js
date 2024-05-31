eb41a65fd05c1bfe5604b1ae9996328904af6a45
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-05-31 19:26:54 +0300

    Refactoring of code

--- 3rdTask.js
+++ 3rdTask.js
@@ -1,72 +1,64 @@
-class Stock {
-  constructor(name, description, ticker, isin, country, exchange) {
+class Security {
+  constructor(name, description, ticker, isin) {
     this.name = name;
     this.description = description;
     this.ticker = ticker;
     this.isin = isin;
-    this.country = country;
-    this.exchange = exchange;
+    this.type = "";
   }
 
   getName() {
-    return this.name;
+    return `Название ${this.type}: ${this.name}`
   }
 
   getDescription() {
-    return this.description;
+    return `Описание ${this.type}: ${this.name}`
   }
 
   getTicker() {
-    return this.ticker;
+    return `Тикер ${this.type}: ${this.name}`
   }
 
   getIsin() {
-    return this.isin;
+    return `ИСИН ${this.type}: ${this.name}`
+  }
+
+}
+
+class Stock extends Security {
+  constructor(name, description, ticker, isin, country, exchange) {
+    super(name, description, ticker, isin)
+    this.country = country;
+    this.exchange = exchange;
+    this.type = "акции"
   }
 
   getCountry() {
-    return this.country;
+    return `Страна ${this.type}: ${this.country}`;
   }
 
   getExchange() {
-    return this.exchange;
+    return `Биржа ${this.type}: ${this.exchange}`;
   }
 }
 
-class Bond {
+class Bond extends Security {
   constructor(name, description, ticker, isin, nominal, coupon) {
-    this.name = name;
-    this.description = description;
-    this.ticker = ticker;
-    this.isin = isin;
+    super(name, description, ticker, isin)
     this.nominal = nominal;
     this.coupon = coupon;
-  }
-
-  getName() {
-    return this.name;
-  }
-
-  getDescription() {
-    return this.description;
-  }
-
-  getTicker() {
-    return this.ticker;
-  }
-
-  getIsin() {
-    return this.isin;
+    this.type = "облигации"
   }
 
   getNominal() {
-    return this.nominal;
+    return `Номинал ${this.type}: ${this.nominal}`;
   }
 
   getCoupon() {
-    return this.exchange;
+    `Размер купона ${this.type}: ${this.coupon}`
   }
 }
+
 
 let new_bond = new Bond("ОФЗ 29009", "Описание", "SU29009RMFS6", "RU000A0JV4N8", "1000", "50");
 
